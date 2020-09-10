bdb21af5e5b1ddd10c31c3e9e7a54f45b88800cf
aSilas <silastj@hotmail.com>
2020-09-10 01:55:30 -0300

    Dados do filme em destaque

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import React, {useEffect, useState} from 'react';
 import Tmdb from './helpers/Tmdb';
 import MovieRow from './components/MovieRow';
 import './App.css';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [ featureData, setFeatureData] = useState(null);
 
   useEffect(()=> {
     const loadAll = async () => {
-
+      //Pegando a lista Total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //Pegando o Featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      //Pegando o item 
+      let chosen = originals[0].items.results[randomChosen];
+
+      //Função de pegar um filme
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      //Armazenar na minha state
+      // console.log(chosenInfo);
+      setFeatureData(chosenInfo);
     }
 
     loadAll();
@@ -21,6 +35,11 @@ export default () => {
 
   return(
     <div className="page">
+      {featureData &&
+        <FeaturedMovie item={featureData}/>
+      }
+
+      
       <section className="lists">
         {movieList.map((item, key)=>(
           <div>
--- src/helpers/Tmdb.js
+++ src/helpers/Tmdb.js
@@ -12,7 +12,7 @@ export default {
     getHomeList:async () => {
         return[
             {
-                slug:'originais',
+                slug:'originals',
                 title:'Originais do Netflix',
                 items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
             },
@@ -52,5 +52,28 @@ export default {
                 items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             },
         ];
+    },
+
+    //APENAS UM FILME
+    getMovieInfo:async (movieId, type) => {
+        let info ={};
+
+        //verificação se caso seja movie ou tv
+        if(movieId){
+            switch(type){
+                case'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`)
+                break;
+                case'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`)
+                break;
+                //só para o navagedor não reclamar
+                default:
+                    info = null;
+                break;
+            }
+        }
+
+        return info;
     }
 }
